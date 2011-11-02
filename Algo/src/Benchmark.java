30dbf1a819cc9db4752e2bcf0da086ce1d248b7c
romainrince <romainrince@d787ed9e-0793-1b28-2ab0-8b5d3b8d7750>
2011-11-02 17:18:53 +0000

    New Benchmark

--- Algo/src/Benchmark.java
+++ Algo/src/Benchmark.java
@@ -1,83 +1,141 @@
-
+import java.util.ArrayList;
 
 public class Benchmark
 {
 
-	public static void main(String[] args)
+	public static void bench(GrapheOriente graphe, int n)
 	{
 		long mili;
-		ListGraph grapheListe10 = new ListGraph();
-		ListGraph grapheListe100 = new ListGraph();
-		ListGraph grapheListe1000 = new ListGraph();
-		
-		System.out.println("===CREATION DES NOEUDS===");
-		System.out.println("Creation noeuds ListGraph10");
-		mili=System.currentTimeMillis();
-		creerNoeuds(grapheListe100, 10);
+		boolean verif;
+		ArrayList<Integer> listeId = new ArrayList<Integer>();
+
+		int i;
+
+		System.out.println("===CREATION DES " + n + " NOEUDS===");
+
+		mili = System.currentTimeMillis();
+		creerNoeuds(graphe, n);
 		mili = System.currentTimeMillis() - mili;
-		System.out.println("temps = "+ mili);
-		
-		System.out.println("Creation noeuds MatGraph10");
-		
-		System.out.println("Creation noeuds ListGraph100");
-		mili=System.currentTimeMillis();
-		creerNoeuds(grapheListe100, 1000);
+		System.out.println("temps = " + mili);
+
+		System.out.println("===CREATION DES " + ((n * n + n) / 2) + " ARCS===");
+
+		mili = System.currentTimeMillis();
+		listeId = creerArcs(graphe, n);
 		mili = System.currentTimeMillis() - mili;
-		System.out.println("temps = "+ mili);
-		
-		System.out.println("Creation noeuds MatGraph100");
-		
-		System.out.println("Creation noeuds ListGraph1000");
-		mili=System.currentTimeMillis();
-		creerNoeuds(grapheListe1000, 1000);
+		System.out.println("temps = " + mili);
+
+		System.out.println("===LISTE DES NOEUDS===");
+
+		mili = System.currentTimeMillis();
+		ArrayList<Integer> listeNoeuds = graphe.listeNoeuds();
 		mili = System.currentTimeMillis() - mili;
-		System.out.println("temps = "+ mili);
-		
-		System.out.println("Creation noeuds MatGraph1000");
-		
-		System.out.println(grapheListe10.listeNoeuds());//TODO remove
-		System.out.println("===CREATION DES ARCS===");
-		System.out.println("Creation Arcs ListGraph10");
-		mili=System.currentTimeMillis();
-		creerArcs(grapheListe10, 10);
+		System.out.println("temps = " + mili);
+		System.out.println(listeNoeuds);
+
+		System.out.println("===LISTE DES ARCS===");
+
+		mili = System.currentTimeMillis();
+		ArrayList<Arc> listeArcs = graphe.listeArcs();
 		mili = System.currentTimeMillis() - mili;
-		
-		System.out.println("Creation Arcs MatGraph10");
-		System.out.println("Creation Arcs ListGraph100");
-		mili=System.currentTimeMillis();
-		creerArcs(grapheListe100, 100);
+		System.out.println("temps = " + mili);
+
+		TestMonGraphe test = new TestMonGraphe(graphe);
+
+		System.out.println("===ACYCLIQUE===");
+
+		mili = System.currentTimeMillis();
+		verif = test.acyclicite();
 		mili = System.currentTimeMillis() - mili;
+		System.out.println(verif);
+		System.out.println("temps = " + mili);
+
+		System.out.println("===SIMPLE===");
 		
-		System.out.println("Creation Arcs MatGraph100");
-		System.out.println("Creation Arcs ListGraph1000");
-		mili=System.currentTimeMillis();
-		creerArcs(grapheListe1000, 1000);
+		mili = System.currentTimeMillis();
+		verif = test.simple();
 		mili = System.currentTimeMillis() - mili;
+		System.out.println(verif);
+		System.out.println("temps = " + mili);
+
+		System.out.println("===CONNEXE===");
+
+		mili = System.currentTimeMillis();
+		verif = test.connexite();
+		mili = System.currentTimeMillis() - mili;
+		System.out.println(verif);
+		System.out.println("temps = " + mili);
+
+		mili = System.currentTimeMillis();
+		test.afficheParcours(0, 0, true);
+		mili = System.currentTimeMillis() - mili;
+		System.out.println("\ntemps = " + mili);
+
+		mili = System.currentTimeMillis();
+		test.afficheParcours(1, 0, true);
+		mili = System.currentTimeMillis() - mili;
+		System.out.println("\ntemps = " + mili);
 		
-		System.out.println("Creation Arcs MatGraph1000");
-		
-		// TODO grapheMatrice
+		System.out.println("===SUPPRESSION DES ARCS===");
+
+		mili = System.currentTimeMillis();
+		supprimerArcs(graphe, n, listeId);
+		mili = System.currentTimeMillis() - mili;
+		System.out.println("temps = " + mili);
+
+		System.out.println("Graphe en cours de création. Veuillez patienter...");
+
+		listeId = creerArcs(graphe, n);
+
+		System.out.println("===SUPPRESSION DES NOEUDS===");
+
+		mili = System.currentTimeMillis();
+		supprimerNoeuds(graphe, n);
+		mili = System.currentTimeMillis() - mili;
+		System.out.println("temps = " + mili);
+
+
+
 	}
 
-	public static void creerNoeuds(GrapheOriente graph, int nbNoeuds)
+	private static void creerNoeuds(GrapheOriente graph, int nbNoeuds)
 	{
 		for (int i = 0; i < nbNoeuds; i++)
 		{
 			graph.ajouterN(i);
 		}
-		System.out.println(graph.listeNoeuds());//TODO remove
 	}
-	
-	public static void creerArcs(GrapheOriente graph, int nbNoeuds)
+
+	private static ArrayList<Integer> creerArcs(GrapheOriente graph, int nbNoeuds)
+	{
+		ArrayList<Integer> listeId = new ArrayList<Integer>();
+		for (int i = 0; i < nbNoeuds; i++)
+		{
+			for (int j = i + 1; j < nbNoeuds; j++)
+			{
+				listeId.add(graph.ajouterA(i, j));
+			}
+		}
+		return listeId;
+	}
+
+	private static void supprimerNoeuds(GrapheOriente graph, int nbNoeuds)
 	{
 		for (int i = 0; i < nbNoeuds; i++)
 		{
-			for (int j = i+1; j < nbNoeuds; j++)
-			{
-				System.out.println(i+" "+j);//TODO remove
-				graph.ajouterA(i,j);
-			}
+			graph.supprimerN(i);
 		}
 	}
 
+	private static void supprimerArcs(GrapheOriente graph, int nbNoeuds, ArrayList<Integer> listeId)
+	{
+		int k = 0;
+		for (int i = 0; i < nbNoeuds; i++)
+		{
+			for (int j = i + 1; j < nbNoeuds; j++)
+			{
+				graph.supprimerA(listeId.get(k), i, j);
+			}
+		}
+	}
 }
